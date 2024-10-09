3087a8fa48cf05e961afac6f78a4a55cba5cc74c
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-10-09 14:20:32 +0300

    fix

--- src/app/shared/components/reviews/reviews.component.ts
+++ src/app/shared/components/reviews/reviews.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.scss']
+  styleUrls: ['./reviews.component.scss'],
 })
 export class ReviewsComponent implements OnInit {
-
-  constructor() { }
+  mobile = false;
+  constructor() {}
 
   ngOnInit(): void {
+    this.checkScreenSize();
+    window.addEventListener('resize', this.checkScreenSize.bind(this));
   }
 
+  checkScreenSize(): void {
+    this.mobile = window.innerWidth <= 430;
+  }
 }
